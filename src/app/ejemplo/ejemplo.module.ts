f7c3e9e3c969a6ca2b63bf0a7f91b43bf6f2ac26
“anacristinarodriguez” <ac.rodriguezd1@uniandes.edu.co>
2023-11-28 07:50:47 -0500

    ejemplo

---
+++ src/app/ejemplo/ejemplo.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EjemploComponent } from './ejemplo.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EjemploComponent]
+})
+export class EjemploModule { }
